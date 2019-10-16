222676cbfb3d523a3df3756f67fde50065671bf5
Pulkit2975 <54947834+Pulkit2975@users.noreply.github.com>
2019-10-16 08:30:20 +0530

    Question 24 Java Programming

---
+++ pql.java
@@ -0,0 +1,15 @@
+
+public class pql {
+    public static void main(String args[])
+    {
+        public static void copies3(string a[]) 
+        {
+        for(int i=0;i<=a.length;i++)
+        {
+            String x=a[i];
+            a[i]=x+x+x;   
+        }
+        System.out.println(a);
+        } 
+    }
+}
